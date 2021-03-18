a105a471890f9870a70e3f068547ea70cf72b2ee
matteo <matte.rubbiani@gmail.com>
2021-03-18 12:25:36 +0100

    h

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+io.on('connection', socket => {
+  socket.on("move", data => {
+    io.emit("move", data)
+  })
+})
+
+http.listen(3003)
