ab02340d84a1e520d328b5f4be7a5d548f7539dd
matteo <matte.rubbiani@gmail.com>
2021-03-19 07:32:52 +0100

    h

--- app.js
+++ app.js
@@ -5,9 +5,11 @@ const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
 io.on('connection', socket => {
+  console.log("connecteeed")
   socket.on("move", data => {
     io.emit("move", data)
   })
 })
 
 http.listen(3003)
+console.log("running")
