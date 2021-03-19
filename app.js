1b9738319a2ececfc06447bf642bf815b6fddd80
root <root@mail.sheetcheat.xyz>
2021-03-19 10:42:01 +0000

    h

--- app.js
+++ app.js
@@ -4,10 +4,20 @@ const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
+let lastId = null;
 io.on('connection', socket => {
   console.log("connecteeed")
   socket.on("move", data => {
     socket.broadcast.emit("move", data)
+  })
+  
+  socket.on("new-player", ()=>{
+    socket.emit("new-player", lastId ? lastId+1 : 0);
+    lastId = lastId ? lastId+1 : 0;
+  })
+
+  socket.on("get-game", ()=>{
+    return lastId ? lastId+1 : 0;
   })
 })
 
