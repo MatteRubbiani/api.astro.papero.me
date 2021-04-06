b20c3b7550ea528fcf06d26dbfe72864b5647782
matteo <matte.rubbiani@gmail.com>
2021-04-06 12:32:58 +0200

    H

--- app.js
+++ app.js
@@ -199,8 +199,6 @@ io.on('connection', socket => {
     let game = await ActiveGames.getActiveGameById(user.gameId)
     if (!game) return null
     if (game.status >= 1){
-      console.log("killing user")
-      console.log(socket)
       game.changePlayerStatus(user.userId, 0)
       let data = {
         localId: game.getPlayerById(user.userId).localId,
