6629c7ce48fe52dd691a202fb289ac499b9477a5
matteo <matte.rubbiani@gmail.com>
2021-04-09 12:49:19 +0200

    H

--- managers/activeGames.js
+++ managers/activeGames.js
@@ -212,6 +212,11 @@ class ActiveGames{
     endTurn(){
         ActiveGames.gamesStatus[this.id] += .5
         this.status += .5
+        for (let i=0; i<this.players.length; i++){
+            this.players[i].status = 2
+            this.players[i].from = this.players[i].to
+            this.players[i].to = this.players[i].from
+        }
         //check if game has ended
         if (this.gameEnded()){
             this.timer = Infinity
